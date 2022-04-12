4115be0d7c7db8e184218f14f6dc40a7b1a1af17
drlkmt <drlkmt@outlook.com>
2022-04-12 03:35:54 +0300

    third

--- idz3/main.c
+++ idz3/main.c
@@ -1,12 +1,18 @@
-/* Отсутствует проверка даты месяца и года на корректность
+﻿/* Отсутствует проверка даты месяца и года на корректность
  * дата не должна быть 59 или 99 или 199 и тд. Тоже самое
  * и с месяцем
- */                        
+ */
 
 #include <iostream>
 #define THISMONTH 4
 #define THISYEAR 2022
 #define NUMBEROBJINARR 5
+#define WINTERCODE 0x1
+#define SUMMERCODE 0x2
+#define AUTUMNCODE 0x3
+#define SPRINGCODE 0x4
+
+int monthDate[] = { 31,28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 class Date
 {
@@ -15,28 +21,28 @@ private:
 public:
 	Date(char strDate[])
 	{
-		
+
 		char c;
 		int i = 0, dotcx = 0;
-		for(c = 0; (c = strDate[i]) != '\0'; i++)
-		{	
-			if(c == '.') dotcx++;
+		for (c = 0; (c = strDate[i]) != '\0'; i++)
+		{
+			if (c == '.') dotcx++;
 			else
 			{
-				switch(dotcx)
+				switch (dotcx)
 				{
-					case 0:
-						day *= 10;
-						day += c - '0';
-						break;
-					case 1:
-						month *= 10;
-						month += c - '0';
-						break;
-					case 2:
-						year *= 10;
-						year += c - '0';
-						break;
+				case 0:
+					day *= 10;
+					day += c - '0';
+					break;
+				case 1:
+					month *= 10;
+					month += c - '0';
+					break;
+				case 2:
+					year *= 10;
+					year += c - '0';
+					break;
 				}
 			}
 		}
@@ -55,7 +61,7 @@ public:
 		month = THISMONTH;
 		year = THISYEAR;
 	}
-	
+
 	Date()
 	{
 		day = 1;
@@ -68,14 +74,74 @@ public:
 		printf("Day is: %d\tMonth is: %d\tYear is:%d\n", day, month, year);
 	}
 
-	int getMonth()
+	int getSeason()
 	{
-		return month;
+		switch (month)
+		{
+		case 12:
+		case 1:
+		case 2:
+			return WINTERCODE;
+			break;
+
+		case 6:
+		case 7:
+		case 8:
+			return SUMMERCODE;
+			break;
+
+		case 9:
+		case 10:
+		case 11:
+			return AUTUMNCODE;
+			break;
+
+		case 3:
+		case 4:
+		case 5:
+			return SPRINGCODE;
+			break;
+		}
 	}
+
+	int getDayOfWeek()
+	{
+		return day % 7;
+	}
+
+	int getDecade()
+	{
+		if (day <= 10) return 1;
+		else if (day > 10 && day < 20) return 2;
+		else return 3;
+	}
+
+	bool isItLeapYear()
+	{
+		if (year % 4 == 0)
+		{
+			if (year % 100 == 0)
+			{
+				if (year % 400 == 0) return 1;
+				else return 0;
+			}
+			else return 1;
+		}
+		else return 0;
+	}
+
+	int getNumOfDateMonth()
+	{
+		if (isItLeapYear())
+		{
+			monthDate[1] += 1;
+			return monthDate[month - 1];
+		}
+		else return monthDate[month - 1];
+	}
+
 };
 
-
-void isItSpring(Date dateArr[]);
 
 int main()
 {
@@ -83,26 +149,23 @@ int main()
 	Date* dateArr = (Date*)malloc(sizeof(Date) * NUMBEROBJINARR);
 	dateArr[0] = Date();
 	dateArr[1] = Date(2);
-	dateArr[2] = Date(29,4,2007);
-	dateArr[3] = Date(14,5,2003);
+	dateArr[2] = Date(29, 4, 2007);
+	dateArr[3] = Date(14, 5, 2003);
 	dateArr[4] = Date(dateBuffer);
 
-	isItSpring(dateArr);
-
-	return 0;
-}
-
-void isItSpring(Date dateArr[])
-{
-	for(int i = 0; i < NUMBEROBJINARR; i++)
+	for (int i = 0; i < NUMBEROBJINARR; i++)
 	{
-		int month = dateArr[i].getMonth();
-		if(month >= 3 && month <= 5)
-		{	
+		if (dateArr[i].getSeason() == SPRINGCODE)
+		{
 			printf("It is Spring --> ");
 			dateArr[i].printDate();
 		}
 		else
+		{
 			dateArr[i].printDate();
+		}
 	}
+
+
+	return 0;
 }
